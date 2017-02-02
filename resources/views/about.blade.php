5108dd20e1860d9366b8c9ed25f64858d97db893
Roberts Gulans <rgulans@gmail.com>
2017-02-02 21:53:44 +0200

    Another route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>About us</title>
+  </head>
+  <body>
+    Abouuuuut
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </body>
+</html>
